097f66678c34065ab305937580d4fefb80800a26
Liep Boeger <liepboeger@hotmail.com>
2023-09-13 19:51:02 -0300

    dynamic routes

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Liep Boeger',
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Liep Boeger',
     })
